57c3eea7872409499ebdea3d5657aab76c550fca
vul3cl41up <138540058+vul3cl41up@users.noreply.github.com>
2023-09-19 06:20:39 +0800

    完成 技能選擇面板

---
+++ Assets/Bin/Model_scene_3/Script/Character_Data.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Model_scene_3
+{
+    [CreateAssetMenu(fileName = "Character_Data", menuName = "Data/Character_Data")]
+    public class Character_Data : ScriptableObject
+    {
+        public float blood;
+        public float blood_now;
+        public float magic;
+        public float magic_now;
+
+        public float move_speed;
+        public float attack_speed;
+
+        public Skill_Data skill_1;
+        public Skill_Data skill_2;
+        public Skill_Data skill_3;
+        public Skill_Data skill_4;
+    }
+}
--- Assets/Bin/Model_scene_3/Script/Skill_Panel_Control.cs
+++ Assets/Bin/Model_scene_3/Script/Skill_Panel_Control.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 namespace Model_scene_3
 {
@@ -15,6 +16,7 @@ namespace Model_scene_3
         public TMP_Text title;
         public TMP_Text description;
         public GameObject selected;
+        public Character_Data character_data;
         EventSystem eventSystem;
 
         int choose_count = 0;
@@ -33,38 +35,35 @@ namespace Model_scene_3
         private void Update()
         {
             selected = eventSystem.currentSelectedGameObject;
-            if (selected == null)
-            {
-                selected = skill_grid.transform.GetChild(0).gameObject;
-                eventSystem.SetSelectedGameObject(selected);
-            }
-            if(selected.active == false)
-            {
-                selected = skill_grid.transform.GetChild(choose_count).gameObject;
-                eventSystem.SetSelectedGameObject(selected);
-            }
-            if (selected.CompareTag("Skill_button"))
-            {
-                DisplayDescription();
-                Choose();
-            }
-            if (selected.CompareTag("Skill_selected_button"))
-            {
-                DisplayDescription();
-            }
-
+            DisplayDescription();
+            Choose();
+            Cancel();
         }
 
         private void DisplayDescription()
         {
-            Skill_Data selected_skill_data = selected.GetComponent<Skill_Data_Control>().this_skill_data;
-            title.text = selected_skill_data.skill_name;
-            description.text = selected_skill_data.skill_description;
+            if (selected == null)
+            {
+                selected = skill_grid.transform.GetChild(choose_count).gameObject;
+                eventSystem.SetSelectedGameObject(selected);
+            }
+            if (selected.activeSelf == false)
+            {
+                selected = skill_grid.transform.GetChild(choose_count).gameObject;
+                eventSystem.SetSelectedGameObject(selected);
+            }
+            if (selected.CompareTag("Skill_button") || selected.CompareTag("Skill_selected_button") || selected.CompareTag("Common_attack"))
+            {
+                Skill_Data selected_skill_data = selected.GetComponent<Skill_Data_Control>().this_skill_data;
+                title.text = selected_skill_data.skill_name;
+                description.text = selected_skill_data.skill_description;
+            }
+
         }
 
         public void Choose()
         {
-            if (Input.GetKeyDown(KeyCode.Z) && choose_count <= 4)
+            if (Input.GetButtonDown("Submit") && selected.CompareTag("Skill_button") && choose_count <= 4)
             {
                 GameObject new_skill_selected_slot = Instantiate(skill_selected_slot, Vector3.zero, Quaternion.identity, skill_grid.transform);
                 new_skill_selected_slot.GetComponent<Skill_Data_Control>().this_skill_data = selected.GetComponent<Skill_Data_Control>().this_skill_data;
@@ -74,6 +73,35 @@ namespace Model_scene_3
             
         }
 
+        public void Cancel()
+        {
+            if (Input.GetButtonDown("Submit") && selected.CompareTag("Skill_selected_button") && choose_count > 0)
+            {
+                int index = selected.GetComponent<Skill_Data_Control>().this_skill_data.skill_index;
+                skill_pool.transform.GetChild(index).gameObject.SetActive(true);
+                Destroy(selected);
+                choose_count--;
+            }
+            if(Input.GetButtonDown("Cancel") && choose_count > 0)
+            {
+                selected = skill_grid.transform.GetChild(choose_count).gameObject;
+                int index = selected.GetComponent<Skill_Data_Control>().this_skill_data.skill_index;
+                skill_pool.transform.GetChild(index).gameObject.SetActive(true);
+                Destroy(selected);
+                choose_count--;
+            }
+        }
 
+        public void StartGame()
+        {
+            if(choose_count == 4)
+            {
+                character_data.skill_1 = skill_grid.transform.GetChild(1).gameObject.GetComponent<Skill_Data_Control>().this_skill_data;
+                character_data.skill_2 = skill_grid.transform.GetChild(2).gameObject.GetComponent<Skill_Data_Control>().this_skill_data;
+                character_data.skill_3 = skill_grid.transform.GetChild(3).gameObject.GetComponent<Skill_Data_Control>().this_skill_data;
+                character_data.skill_4 = skill_grid.transform.GetChild(4).gameObject.GetComponent<Skill_Data_Control>().this_skill_data;
+                SceneManager.LoadScene(2);
+            }
+        }    
     }
 }
